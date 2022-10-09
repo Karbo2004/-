6f6ff2a0799ed94102f219d5e089cb8931aed4fc
богдан <113586104+Karbo2004@users.noreply.github.com>
2022-10-09 22:50:04 +0300

    Update programma.py

--- project1/programma.py
+++ project1/programma.py
@@ -10,28 +10,23 @@ def main(req):
  if 'Как добраться' in req.lower() or 'Куда ехать' in req.lower() or 'Место' in req.lower():
         Doroga()
 
-
 def Raspisanie():
     print('Расписание:')
     print('Среда - 19:00')
     print('Пятница - 19:30')
     print('Воскресенье - 17:00')
 
-
 def Trener():
     print('Тренер:')
     print('Номер: 88005553535')
-
 
 def Oplata():
     print('оплата:')
     print('500р.')
 
-
 def Poseshaemost():
     print('Дни посещения:')
     print('Среда, пятница, воскресенье')
-
 
 def Doroga():
     print('Вы можете узнать адресс и маршрут в приложении')
