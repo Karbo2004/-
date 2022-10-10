f5c694233ee5639e7174090935b0824dbd023ba5
богдан <113586104+Karbo2004@users.noreply.github.com>
2022-10-10 09:41:50 +0300

    Update programma.py

--- project1/programma.py
+++ project1/programma.py
@@ -1,32 +1,27 @@
 def main(req):
- if 'Расписание занятий' in req() or 'Дни недели' in req():
+     if 'расписание занятий' in req or 'дни недели' in req:
         Raspisanie()
- if 'Тренер' in req() or 'Контакты тренера' in req():
+     elif 'тренер' in req or 'контакты тренера' in req:
         Trener()
- if 'Оплата' in req() or 'Стоимость' in req() or 'Сумма' in req():
+     elif 'оплата' in req or 'стоимость' in req or 'сумма' in req:
         Oplata()
- if 'Посещаемость' in req() or 'Пропуск' in req() or 'Режим' in req():
+     elif 'посещаемость' in req or 'пропуск' in req or 'режим' in req:
         Poseshaemost()
- if 'Как добраться' in req() or 'Куда ехать' in req() or 'Место' in req():
+     elif 'как добраться' in req or 'куда ехать' in req or 'место' in req:
         Doroga()
-
 def Raspisanie():
-    print('Расписание:')
-    print('Среда - 19:00')
-    print('Пятница - 19:30')
-    print('Воскресенье - 17:00')
-
+     print('расписание:')
+     print('среда - 19:00')
+     print('пятница - 19:30')
+     print('воскресенье - 17:00')
 def Trener():
-    print('Тренер:')
-    print('Номер: 88005553535')
-
+     print('тренер:')
+     print('номер: 88005553535')
 def Oplata():
-    print('оплата:')
-    print('500р.')
-
+     print('оплата:')
+     print('500р.')
 def Poseshaemost():
-    print('Дни посещения:')
-    print('Среда, пятница, воскресенье')
-
+     print('дни посещения:')
+     print('среда, пятница, воскресенье')
 def Doroga():
-    print('Вы можете узнать адресс и маршрут в приложении')
+     print('вы можете узнать адресс и маршрут в приложении')
